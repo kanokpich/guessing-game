b2a1ad93ab26f12c2049da95aca5516804fd5461
kanokpich <60098281+kanokpich@users.noreply.github.com>
2022-12-07 17:40:54 +0700

    [backend] guess api

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"math/rand"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type LoginBody struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -13,5 +20,33 @@ type LoginResponse struct {
 	Data AuthTokenDta `json:"data"`
 }
 
+type GuessedNumber struct {
+	GuessedNumber int `json:"GuessedNumber"`
+}
+
+var randomNumber int = rand.Intn(100)
+
+func getNumber(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, randomNumber)
+}
+
+func guessNumber(c *gin.Context) {
+	var inputNumber GuessedNumber
+
+	if err := c.BindJSON(&inputNumber); err != nil {
+		return 
+	}
+
+	if inputNumber.GuessedNumber == randomNumber {
+		c.IndentedJSON(http.StatusCreated, "Guess correct")
+		randomNumber = rand.Intn(100)
+	}
+}
+
 func main() {
+	router := gin.Default()
+	router.GET("/number", getNumber)
+	router.POST("/guess", guessNumber)
+
+	router.Run("localhost:8000")
 }
